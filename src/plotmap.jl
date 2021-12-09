9900860c75f081b73b754fccd520059b2d826ad7
Jost von Hardenberg <hardenberg@gmail.com>
2021-12-09 23:43:44 +0100

    added borders

--- src/plotmap.jl
+++ src/plotmap.jl
@@ -12,36 +12,34 @@ Plots variable `var` from a netcdf file `fname`.
 Optional arguments are available to control the details of the plot.
 
 # Arguments
-- 'fname::String': netcdf filename containing the data to plot
-- 'var'::String  : name of the variable to plot
+* data::Array{Float32,2} : data to plot. If a 3D array is passed, only the first frame is plotted: `data[:,:,1]`
+* lon::Array{Float64,1} : longitudes
+* lat::Array{Float64,1} : latitudes
 
 Optional:
-- 'lon::String': name of the lon variable ("lon")
-- 'lat::String': name of the lat variable ("lat")
-- 'lonb::String': name of the lon bounds variable ("lon_bnds")
-- 'latb::String': name of the lat boundsvariable ("lat_bnds")
-- 'title':      title string ("")
-- 'cstep': 	 divisions of the colorbar axis ([])
-- 'cmap':        colormap ("RdBu_r")
-- 'proj':        projection. One of ["platecarree", "robinson", "mollweide"]. Defaults to "platecarree".
-- 'cpad':        padding (shift) of the colorbar (0.08)
-- 'sub':         matplotlib subplot option (e.g. "221" for the first panel of 2x2 subplots) ("111")
-- 'clabel':      label of the colorbar (defaults to the units string read from the netcdf file)
-- 'cdir':        direction of the colorbar. One of ["horizontal", "vertical"]. ("horizontal")
-- 'cscale':      scaling of the colorbar (0.65)
-- 'cfs':         colorbar ticks font size (12)
-- 'lfs':         colorbar label font size (12)
-- 'tfs':         title font size (14)
-- 'tpad':        padding (shift) of the title string
-- 'tweight':     weight of title font. One of [ 'normal" "bold" "heavy" "light" "ultrabold" "ultralight"]. ("normal")
-- 'grid':        grid spacing (defaults to [60,30]). set to empty [] to remove gridlines.
-- 'region::NTuple{4,Int64}':       region to plot in format (lon1, lon2, lat1, lat2). Defaults to global.
-- 'style':       one of ["pcolormesh" "contourf"]. Defaults to "pcolormesh".
-- 'levels':      contour plot levels. Can be an array or a number of levels (auto)
-- 'extend':      plot levels outside `levels` range. One of ["neither", "both", "min", "max"]. Default: "neither".
+* title :       title string ("")
+* cstep :       divisions of the colorbar axis ([])
+* cmap :        colormap ("RdBu_r")
+* proj :        projection. One of ["platecarree", "robinson", "mollweide"]. Defaults to "platecarree".
+* cpad :        padding (shift) of the colorbar (0.08)
+* sub :         matplotlib subplot option (e.g. "221" for the first panel of 2x2 subplots) ("111")
+* clabel :      label of the colorbar (defaults to the units string read from the netcdf file)
+* cdir :        direction of the colorbar. One of ["horizontal", "vertical"]. ("horizontal")
+* cscale :      scaling of the colorbar (0.65)
+* cfs :         colorbar ticks font size (12)
+* lfs :         colorbar label font size (12)
+* tfs :         title font size (14)
+* borders :     national borders (false)
+* tpad :        padding (shift) of the title string
+* tweight :     weight of title font. One of [ 'normal" "bold" "heavy" "light" "ultrabold" "ultralight"]. ("normal")
+* grid :        grid spacing (defaults to [60,30]). set to empty [] to remove gridlines.
+* region::NTuple{4,Int64}:       region to plot in format (lon1, lon2, lat1, lat2). Defaults to global.
+* style :       one of ["pcolormesh" "contourf"]. Defaults to "pcolormesh".
+* levels :      contour plot levels. Can be an array or a number of levels (auto)
+* extend :      plot levels outside `levels` range. One of ["neither", "both", "min", "max"]. Default: "neither".
 
 """
-function plotmap(fname::String, var::String; lon="lon", lat="lat", lonb="lon_bnds", latb="lat_bnds", title="", cstep=[], cmap="RdBu_r", proj="", cpad=0.08, tpad=24, sub=111, clabel="NONE", cdir="horizontal", cscale=0.65, tfs=14, cfs=12, lfs=12, tweight="normal", grid=[60,30], region=(), style="pcolormesh", levels=0, extend="neither")
+function plotmap(fname::String, var::String; lon="lon", lat="lat", lonb="lon_bnds", latb="lat_bnds", titles="", title="", cstep=[], cmap="RdBu_r", proj="", cpad=0.08, tpad=24, sub=111, clabel="NONE", cdir="horizontal", cscale=0.65, tfs=14, cfs=12, lfs=12, tweight="normal", grid=[60,30], region=(), style="pcolormesh", levels=0, extend="neither", borders=false)
 
 # pcolormesh needs cell boundaries
 if style=="pcolormesh"
@@ -66,7 +64,7 @@ data=ncread(fname, var);
 units=ncgetatt(fname, var, "units")
 if clabel=="NONE" clabel=units end
 
-plotmap(lonv, latv, data; title=title, cstep=cstep, cmap=cmap, proj=proj, cpad=cpad, tpad=tpad, sub=sub, clabel=clabel, cdir=cdir, cscale=cscale, tfs=tfs, cfs=cfs, lfs=lfs, tweight=tweight, grid=grid, region=region, style=style, levels=levels, extend=extend)
+plotmap(lonv, latv, data; title=title, titles=titles, cstep=cstep, cmap=cmap, proj=proj, cpad=cpad, tpad=tpad, sub=sub, clabel=clabel, cdir=cdir, cscale=cscale, tfs=tfs, cfs=cfs, lfs=lfs, tweight=tweight, grid=grid, region=region, style=style, levels=levels, extend=extend, borders=borders)
 
 end
 
@@ -78,33 +76,34 @@ Plots data in 2D array `data` with longitudes `lon` and latitudes `lat`.
 Optional arguments are available to control the details of the plot.
 
 # Arguments
-- 'data::Array{Float32,2}': data to plot. If a 3D array is passed, only the first frame is plotted: `data[:,:,1]`
-- 'lon::Array{Float64,1}' : longitudes
-- 'lat::Array{Float64,1}' : latitudes
+* data::Array{Float32,2} : data to plot. If a 3D array is passed, only the first frame is plotted: `data[:,:,1]`
+* lon::Array{Float64,1} : longitudes
+* lat::Array{Float64,1} : latitudes
 
 Optional:
-- 'title':      title string ("")
-- 'cstep':       divisions of the colorbar axis ([])
-- 'cmap':        colormap ("RdBu_r")
-- 'proj':        projection. One of ["platecarree", "robinson", "mollweide"]. Defaults to "platecarree".
-- 'cpad':        padding (shift) of the colorbar (0.08)
-- 'sub':         matplotlib subplot option (e.g. "221" for the first panel of 2x2 subplots) ("111")
-- 'clabel':      label of the colorbar (defaults to the units string read from the netcdf file)
-- 'cdir':        direction of the colorbar. One of ["horizontal", "vertical"]. ("horizontal")
-- 'cscale':      scaling of the colorbar (0.65)
-- 'cfs':         colorbar ticks font size (12)
-- 'lfs':         colorbar label font size (12)
-- 'tfs':         title font size (14)
-- 'tpad':        padding (shift) of the title string
-- 'tweight':     weight of title font. One of [ 'normal" "bold" "heavy" "light" "ultrabold" "ultralight"]. ("normal")
-- 'grid':        grid spacing (defaults to [60,30]). set to empty [] to remove gridlines.
-- 'region::NTuple{4,Int64}':       region to plot in format (lon1, lon2, lat1, lat2). Defaults to global.
-- 'style':       one of ["pcolormesh" "contourf"]. Defaults to "pcolormesh".
-- 'levels':      contour plot levels. Can be an array or a number of levels (auto)
-- 'extend':      plot levels outside `levels` range. One of ["neither", "both", "min", "max"]. Default: "neither".
+* title :       title string ("")
+* cstep :       divisions of the colorbar axis ([])
+* cmap :        colormap ("RdBu_r")
+* proj :        projection. One of ["platecarree", "robinson", "mollweide"]. Defaults to "platecarree".
+* cpad :        padding (shift) of the colorbar (0.08)
+* sub :         matplotlib subplot option (e.g. "221" for the first panel of 2x2 subplots) ("111")
+* clabel :      label of the colorbar (defaults to the units string read from the netcdf file)
+* cdir :        direction of the colorbar. One of ["horizontal", "vertical"]. ("horizontal")
+* cscale :      scaling of the colorbar (0.65)
+* cfs :         colorbar ticks font size (12)
+* lfs :         colorbar label font size (12)
+* tfs :         title font size (14)
+* borders :     national borders (false)
+* tpad :        padding (shift) of the title string
+* tweight :     weight of title font. One of [ 'normal" "bold" "heavy" "light" "ultrabold" "ultralight"]. ("normal")
+* grid :        grid spacing (defaults to [60,30]). set to empty [] to remove gridlines.
+* region::NTuple{4,Int64}:       region to plot in format (lon1, lon2, lat1, lat2). Defaults to global.
+* style :       one of ["pcolormesh" "contourf"]. Defaults to "pcolormesh".
+* levels :      contour plot levels. Can be an array or a number of levels (auto)
+* extend :      plot levels outside `levels` range. One of ["neither", "both", "min", "max"]. Default: "neither".
 
 """
-function plotmap(lon, lat, data; title="", cstep=[], cmap="RdBu_r", proj="", cpad=0.08, tpad=24, sub=111, clabel="", cdir="horizontal", cscale=0.65, tfs=14, cfs=12, lfs=12, tweight="normal", grid=[60,30], region=(), style="pcolormesh", levels=0, extend="neither")
+function plotmap(lon, lat, data; title="", titles="", cstep=[], cmap="RdBu_r", proj="", cpad=0.08, tpad=24, sub=111, clabel="", cdir="horizontal", cscale=0.65, tfs=14, cfs=12, lfs=12, tweight="normal", grid=[60,30], region=(), style="pcolormesh", levels=0, extend="neither", borders=false)
 
 #cf = pyimport("cartopy.feature")
 
@@ -139,6 +138,7 @@ end
 
 ccrs = pyimport("cartopy.crs")
 cutil = pyimport("cartopy.util")
+cfeat = pyimport("cartopy.feature")                  
 
 if proj=="robinson"
     proj=ccrs.Robinson()
@@ -154,6 +154,7 @@ end
 ax = subplot(sub, projection=proj)
 if length(region)>0 ax.set_extent(region, crs=ccrs.PlateCarree()) end
 ax.coastlines()
+if borders ax.add_feature(cfeat.BORDERS) end
 xlocvec=vcat(-vcat(grid[1]:grid[1]:180)[end:-1:1], vcat(0:grid[1]:180))
 ylocvec=vcat(-vcat(grid[2]:grid[2]:90)[end:-1:1], vcat(0:grid[2]:90))
 
@@ -176,6 +177,7 @@ end
 
 if length(cstep)>0 clim(cstep[1],cstep[end]); end
 if length(title)>1 PyPlot.title(title, pad=tpad, fontsize=tfs, weight=tweight) end
+if length(titles)>1 PyPlot.title(titles, pad=tpad, fontsize=tfs, weight=tweight) end
 cbar=colorbar(orientation=cdir, extend="both", pad=cpad, label=clabel, shrink=cscale)
 cbar.set_label(label=clabel,size=lfs)
 cbar.ax.tick_params(labelsize=cfs) 
@@ -183,4 +185,3 @@ if length(cstep)>0 cbar.set_ticks(cstep) end
 tight_layout()
 
 end
-                  
